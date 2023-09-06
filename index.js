c65586a994fb53c042d91e0cbbf587c326fc70c3
Philson Nah <phil@Philsons-MacBook-Air.local>
2023-09-06 14:35:16 +0800

    Done

---
+++ index.js
@@ -0,0 +1,72 @@
+// Import all related dependencies
+const ethers = require("ethers");
+const fs = require("fs");
+
+const main = async () => {
+  let addresses = [];
+  try {
+    const ktp = "0xc6C0C0f54a394931a5b224c8b53406633e35eeE7";
+    const totalQty = 750;
+
+    // check if restake file exists
+    if (!fs.existsSync("./addresses.csv")) await storeData("");
+
+    for (let i = 0; i <= totalQty; i++) {
+      const wallet = ethers.Wallet.createRandom();
+      const random = Math.floor(Math.random() * 10) + 1;
+      const x = random / 100000;
+
+      const item = {
+        token_address: ktp,
+        receiver: wallet.address,
+        amount: x,
+      };
+      addresses.push(item);
+    }
+    storeData(addresses);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Data Storage Function
+const storeData = async (addresses) => {
+  const data = formatCSV(addresses);
+  fs.writeFile("./addresses.csv", data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Data stored:", addresses);
+    }
+  });
+};
+
+// Transform JSON to CSV
+const formatCSV = (items) => {
+  let csv = "";
+  // Loop the array of objects
+  for (let row = 0; row < items.length; row++) {
+    let keysAmount = Object.keys(items[row]).length;
+    let keysCounter = 0;
+    // If this is the first row, generate the headings
+    if (row === 0) {
+      // Loop each property of the object
+      for (let key in items[row]) {
+        // This is to not add a comma at the last cell
+        // The '\r\n' adds a new line
+        csv += key + (keysCounter + 1 < keysAmount ? "," : "\r\n");
+        keysCounter++;
+      }
+    } else {
+      for (let key in items[row]) {
+        csv += items[row][key] + (keysCounter + 1 < keysAmount ? "," : "\r\n");
+        keysCounter++;
+      }
+    }
+    keysCounter = 0;
+  }
+  console.log(csv);
+  return csv;
+};
+
+main();
